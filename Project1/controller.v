b325f27ebb286109fb30334ba4f2133543dc3c87
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2020-03-11 12:02:02 +0000

    added files for first project

---
+++ Project1/controller.v
@@ -0,0 +1,18 @@
+
+
+module controller(
+    input clock,
+    input reset,
+    input start,
+    output init,
+    output running,
+    output toggle,
+    output finish,
+    output bist_end
+    
+);
+
+//code here
+
+
+endmodule // controller
