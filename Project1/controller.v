2f5278f50381297c0dd923abdd5d85d4483edc48
Afonso Muralha <afonsomuralha@gmail.com>
2020-03-31 18:40:13 +0100

    New project prototype, added gitignore

--- Project1/controller.v
+++ Project1/controller.v
@@ -1,18 +1,78 @@
 
+// `define NCLOCK 5
 
 module controller(
-    input clock,
+    input clk,
     input reset,
     input start,
     output init,
     output running,
-    output toggle,
+    output reg toggle,
     output finish,
-    output bist_end
-    
+    output reg bist_end
 );
 
-//code here
+reg [3:0] state, next_state; 
+reg [3:0] nclock;
+parameter IDLE=0, INIT=1, RUNNING=2, FINISH=3, END=4;
 
+reg [2:0] ncounter;
+
+always @ (posedge clk) begin
+  if(reset == 1) begin
+    state   <= 0;
+    toggle  <= 0;
+    bist_end <= 0;
+    ncounter <= 0;
+    nclock <= 5;
+  end
+  else
+    state <= next_state;
+end
+
+
+always @(*) begin
+    case (state)
+        IDLE:
+            if(start)
+                next_state = INIT;
+        INIT:
+            next_state = RUNNING;
+        RUNNING:
+            if(ncounter == nclock)
+                next_state = FINISH;
+        FINISH:
+            next_state = IDLE;
+        
+        default:
+            next_state  = IDLE;
+    endcase
+end
+
+assign init = (state == INIT);
+assign running = (state == RUNNING) && (ncounter < nclock);
+assign finish = (state == FINISH); 
+// assign bist_end = (state == END)
+
+always @ (posedge clk) begin
+  if(state == RUNNING) begin
+      if(ncounter <= ncounter) begin
+        toggle = !toggle;
+        ncounter++;
+      end
+      else begin
+          toggle <= 0;
+      end
+  end
+end
+
+always @ (negedge finish, posedge reset, posedge start) begin
+    if(!finish)
+        bist_end <= 1;
+    else if (reset) 
+        bist_end <= 0;
+    else if (start) 
+        bist_end <= 0;
+end
 
 endmodule // controller
---
+++ Project1/controller_tb.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+
+module controller_tb;
+
+    reg CLK;
+    reg START;
+    reg RESET;
+    wire INIT, RUNNING, TOGGLE, FINISH, BIST_END;
+
+    controller uut(
+        .clk(CLK),
+        .reset(RESET),
+        .start(START),
+        .init(INIT),
+        .running(RUNNING),
+        .toggle(TOGGLE),
+        .finish(FINISH),
+        .bist_end(BIST_END)
+    );                 
+    
+    initial
+    begin
+    	$dumpfile("dump.vcd");
+        $dumpvars();
+        
+        CLK = 0;
+    	START = 0;
+    	RESET = 0;
+    end           
+    initial begin
+        #3 RESET = 1;
+        #3 RESET = 0;     
+    
+        
+        #3 START = 1;
+        #3 START = 0;
+    end
+    initial begin
+        #300 $finish;
+    end
+
+    always 
+    #5  CLK =  ! CLK;
+
+endmodule
