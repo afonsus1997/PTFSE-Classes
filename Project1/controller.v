8dce5ded951bca53e8da55f14ed5d214727f6535
pedro <pedro.pinamendes@gmail.com>
2020-03-31 18:42:29 +0100

    novo 30março

--- Project1/controller.v
+++ Project1/controller.v
@@ -8,11 +8,16 @@ module controller(
     output running,
     output toggle,
     output finish,
-    output bist_end
-    
+    output bist_end    
 );
 
 //code here
-
+always @ (posedge clock) begin
+    if(start == 1 && running == 0)
+    begin
+        init <= 1;
+    end
+    else init <= 0;
+end
 
 endmodule // controller
