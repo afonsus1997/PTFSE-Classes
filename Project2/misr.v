def251a4053330635aad24b58c1578e3812d5402
Afonso Muralha <afonsomuralha@gmail.com>
2020-05-18 16:17:17 +0100

    Minor changes

--- Project2/misr.v
+++ Project2/misr.v
@@ -8,7 +8,7 @@ module misr(
 );
 
     parameter NBIT = 6;
-    parameter seed = 6'b111010;
+    parameter seed = 6'b111111;
     reg [NBIT-1:0] dff;
 
     assign signature = dff;
@@ -18,11 +18,11 @@ module misr(
             dff <= seed;
         end
         else begin
-            dff[0] <= grant_o[3]          ^ dff[NBIT-1];
-            dff[1] <= grant_o[2] ^ dff[0] ^ dff[NBIT-1];
-            dff[2] <= grant_o[1] ^ dff[1] ^ dff[NBIT-1];
-            dff[3] <= grant_o[0] ^ dff[2] ^ dff[NBIT-1];
-            dff[4] <=              dff[3] ^ dff[NBIT-1];
+            dff[0] <= grant_o[3] ^ dff[NBIT-1];          
+            dff[1] <= grant_o[2] ^ dff[0];
+            dff[2] <= grant_o[1] ^ dff[1]; 
+            dff[3] <= grant_o[0] ^ dff[2]; 
+            dff[4] <=              dff[3];
             dff[5] <=              dff[4] ^ dff[NBIT-1];
         end
     end
